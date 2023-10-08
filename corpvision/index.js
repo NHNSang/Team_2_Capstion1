8e72ca97779cac88f8377059fe8d2aed0ae13b7d
NHNSang <nguyenhoangnhatsang01061998@gmail.com>
2023-10-08 21:08:21 +0700

    update

--- corpvision/index.js
+++ corpvision/index.js
@@ -18,3 +18,35 @@ document.getElementById('btnCloseOverlaySearch').addEventListener('click', funct
     // đã gán biến cho overlaySearch ở trên với biến toàn cục nên có thể sử dụng
     searchOverlay.style.display = 'none';
 });
+
+// tạo cửa sổ overlay cho nút search
+// gán sự kiện theo dõi cho btnSearch> khi click sẽ thực hiện function được định nghĩa bên dưới
+document.getElementById('btnSearch').addEventListener('click', function(){    
+    //khai báo biến searchOverlay 
+var searchOverlay = document.getElementById('searchOverlay');
+    // sử dụng mệnh đề if
+if(searchOverlay.style.transform === 'scale(0)') {
+    searchOverlay.style.transform = 'scale(100%)';
+}
+else {
+    searchOverlay.style.transform = 'scale(100%)';
+}
+});
+// tạo cửa sổ overlay cho nút search
+
+// chức năng tắt nút close khi không cần search nữa
+document.getElementById('btnCloseOverlaySearch').addEventListener('click', function(){
+    // đã gán biến cho overlaySearch ở trên với biến toàn cục nên có thể sử dụng
+    searchOverlay.style.display = 'none';
+});
+
+
+// Thông báo điền form newsletter
+alert(hello);
+document.getElementById('btnNewletters').onclick = function (){
+    var giaTriEmail = document.getElementById('nhapEmailNewsletter').value;
+    console.log(giaTriEmail);
+    alert(giaTriEmail);
+    
+
+};
